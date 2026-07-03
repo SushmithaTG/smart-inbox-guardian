fce51537303d8e830bf18c103ad92c0d16c3b64b
sushmitha <sushmithathangavel5@gmail.com>
2026-07-03 13:06:41 +0530

    Day 4:Created User model and first JSON API

--- backend/src/main/java/com/sushmitha/smartinboxguardianbackend/controller/HelloController.java
+++ backend/src/main/java/com/sushmitha/smartinboxguardianbackend/controller/HelloController.java
@@ -8,6 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello Smart Inbox Guardian!";
+
+        return "Welcome to Smart Inbox Guardian!";
     }
 }
---
+++ backend/src/main/java/com/sushmitha/smartinboxguardianbackend/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.sushmitha.smartinboxguardianbackend.controller;
+
+import com.sushmitha.smartinboxguardianbackend.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+
+    @GetMapping("/test")
+    public User getUser() {
+
+        User user = new User(
+                1L,
+                "Sushmitha",
+                "sushmithathangavel5@gmail.com"
+        );
+
+        return user;
+    }
+}
