a30fd12dff2a7f51d934e9c77039c98eaaac5741
sushmitha <sushmithathangavel5@gmail.com>
2026-07-04 17:52:40 +0530

    Added @Entity, @Id to User model + updateed application.properties

--- backend/src/main/java/com/sushmitha/smartinboxguardianbackend/controller/UserController.java
+++ backend/src/main/java/com/sushmitha/smartinboxguardianbackend/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.sushmitha.smartinboxguardianbackend.controller;
 
+
 import com.sushmitha.smartinboxguardianbackend.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- backend/src/main/java/com/sushmitha/smartinboxguardianbackend/model/User.java
+++ backend/src/main/java/com/sushmitha/smartinboxguardianbackend/model/User.java
@@ -1,11 +1,28 @@
 package com.sushmitha.smartinboxguardianbackend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+
+//This is called an annotation. It tells Spring Boot:
+//"Store this class in the database.
+@Entity
 public class User {
 
+//    This tells Spring Boot:
+//     "The id field is the primary key
+    @Id
     private Long id;
     private String name;
     private String email;
 
+
+//    This is called the no-argument constructor.
+//    Spring Boot uses it internally when reading data from the database.
+//     Even if you never call it, Spring Boot does.
+    public  User(){
+
+    }
     public User(Long id, String name, String email) {
         this.id = id;
         this.name = name;
