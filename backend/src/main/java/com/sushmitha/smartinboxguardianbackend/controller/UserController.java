fe7942de648824a05ea5c83b2591780645e76d99
sushmitha <sushmithathangavel5@gmail.com>
2026-07-09 22:34:01 +0530

    Day 8:Connected UserController with UserServices and save our first real user into MySQL

--- backend/src/main/java/com/sushmitha/smartinboxguardianbackend/controller/UserController.java
+++ backend/src/main/java/com/sushmitha/smartinboxguardianbackend/controller/UserController.java
@@ -1,25 +1,26 @@
 package com.sushmitha.smartinboxguardianbackend.controller;
 
-
 import com.sushmitha.smartinboxguardianbackend.model.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sushmitha.smartinboxguardianbackend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/users")
+@RequestMapping("/api/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/test")
     public User getUser() {
 
         User user = new User(
-
                 "Sushmitha",
                 "sushmithathangavel5@gmail.com",
                 "testpassword123"
         );
 
-        return user;
+        return userService.saveUser(user);
     }
 }
