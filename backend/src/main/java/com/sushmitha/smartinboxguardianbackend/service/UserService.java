8f418e94ff29edc4fa5c2dd674eee7da07829e7b
sushmitha <sushmithathangavel5@gmail.com>
2026-07-16 23:35:07 +0530

    Day 10: Password encryption added using BCrypt

--- backend/src/main/java/com/sushmitha/smartinboxguardianbackend/service/UserService.java
+++ backend/src/main/java/com/sushmitha/smartinboxguardianbackend/service/UserService.java
@@ -2,6 +2,7 @@ package com.sushmitha.smartinboxguardianbackend.service;
 
 import com.sushmitha.smartinboxguardianbackend.model.User;
 import com.sushmitha.smartinboxguardianbackend.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +14,30 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     // Get all users from database
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
     // Save a new user
     public User saveUser(User user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            return null;
+        }
+        // Encrypt password before saving
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
         return userRepository.save(user);
     }
 
@@ -41,5 +59,16 @@ public class UserService {
 
     public void deleteUser(Long id) {
         userRepository.deleteById(id);
+    }
+
+    public boolean loginUser(String email, String password) {
+
+        User user = userRepository.findByEmail(email);
+
+        if (user == null) {
+            return false;
+        }
+
+        return passwordEncoder.matches(password, user.getPassword());
     }
 }
